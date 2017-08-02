a2d7668d433360f4534b167808709af0f536c2b2
Paradox68 <paradoxscape@live.com>
2017-08-02 16:04:25 -0700

    Update abU-3.js

--- abU-3.js
+++ abU-3.js
@@ -252,7 +252,7 @@
         status: false,
         name: 'Chill Bot',
         loggedInID: null,
-        scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+        scriptLink: 'https://cdn.rawgit.com/Paradox68/AmbientRoom/d230f963/abU-3.js',
         cmdLink: 'http://git.io/245Ppg',
         chatLink: 'https://rawgit.com/Paradox68/AmbientRoom/master/chat.json',
         chat: null,
@@ -263,7 +263,7 @@
             botName: 'Chill Bot',
             language: 'english',
             chatLink: 'https://rawgit.com/Paradox68/AmbientRoom/master/chat.json',
-            scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
+            scriptLink: 'https://cdn.rawgit.com/Paradox68/AmbientRoom/d230f963/abU-3.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
